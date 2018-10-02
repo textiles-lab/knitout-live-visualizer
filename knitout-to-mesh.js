2a8f478c96ec793b157b096cffea7af5ef925190
Jim McCann <ix@tchow.com>
2018-10-01 22:09:24 -0400

    showing something (thanks box-drawing characters). No mesh output. No trasnfers. No yarn tangling.

--- knitout-to-mesh.js
+++ knitout-to-mesh.js
@@ -211,188 +211,370 @@ function parseKnitout(codeText, machine) {
 
 //--------------------------------------
 
-function Edge(type) {
-	this.type = type;
-};
+function Columns() {
+	this.minIndex = Infinity;
+	this.maxIndex = -Infinity;
+	this.storage = [];
+}
 
-Edge.prototype.EMPTY = "edge-empty";
-Edge.prototype.YARN = "edge-yarn";
-Edge.prototype.LOOP = "edge-loop";
+Columns.prototype.getColumn = function Columns_getColumn(i) {
+	if (this.maxIndex < this.minIndex) {
+		this.minIndex = this.maxIndex = i;
+		this.storage = [[]];
+	}
+	while (i < this.minIndex) {
+		this.minIndex -= 1;
+		this.storage.unshift([]);
+	}
+	while (i > this.maxIndex) {
+		this.maxIndex += 1;
+		this.storage.push([]);
+	}
+	return this.storage[i - this.minIndex];
+};
 
 //--------------------------------------
 
-function Face() {
-	this.leftEdges = []; //bottom-to-top
-	this.rightEdges = []; //bottom-to-top
-	this.bottomEdges = []; //left-to-right
-	this.topEdges = []; //left-to-right
-};
+const NEEDLE_REGEX = /^([fb]s?)([-+]?\d+)$/;
 
-//--------------------------------------
+function previousNeedle(n) {
+	let m = n.match(NEEDLE_REGEX);
+	console.assert(m, "previousNeedle must be passed needle; got '" + n + "' instead.");
+	return m[1] + (parseInt(m[2])-1).toString();
+}
 
-function Column() {
-	this.topEdges = []; //edges at the top of the top face (left-to-right)
-	this.leftEdges = []; //edges above faces on the left (bottom-to-top)
-	this.rightEdges = []; //edges above faces on the right (bottom-to-top)
-	this.left = null;
-	this.right = null;
-};
+function nextNeedle(n) {
+	let m = n.match(NEEDLE_REGEX);
+	console.assert(m, "nextNeedle must be passed needle; got '" + n + "' instead.");
+	return m[1] + (parseInt(m[2])+1).toString();
+}
 
-Column.prototype.addFace = function Column_addFace(face) {
-	//link top of column to bottom of face:
-	if (this.topEdges.length !== 0) {
-		console.assert(face.bottomEdges.length === this.topEdges.length, "bottom of face matches top of column (length)");
-		for (let i = 0; i < face.bottomEdges.length; ++i) {
-			this.topEdges[i].link(face.bottomEdges[i]); //will assert() on bad type match
-		}
+
+function needleIndex(n) {
+	let m = n.match(NEEDLE_REGEX);
+	console.assert(m, "needleIndex must be passed needle; got '" + n + "' instead.");
+	return 2*parseInt(m[2]);
+}
+
+function needleBed(n) {
+	let m = n.match(NEEDLE_REGEX);
+	console.assert(m, "needleBed must be passed needle; got '" + n + "' instead.");
+	return m[1];
+}
+
+//the index of the yarn column before needle n if making a stitch in direction d:
+function yarnBeforeIndex(d, n) {
+	if (d === '+') {
+		return needleIndex(n) - 1;
+	} else if (d === '-') {
+		return needleIndex(n) + 1;
 	} else {
-		for (let i = 0; i < face.bottomEdges.length; ++i) {
-			console.assert(face.bottomEdges[i].type === Edge.EMPTY, "bottom of face matches top of empty column");
+		console.assert(d === '+' || d === '-', "Direction must be + or -.");
+	}
+}
+
+//the index of the yarn just after needle n if making a stitch in direction d:
+function yarnAfterIndex(d, n) {
+	if (d === '+') {
+		return needleIndex(n) + 1;
+	} else if (d === '-') {
+		return needleIndex(n) - 1;
+	} else {
+		console.assert(d === '+' || d === '-', "Direction must be + or -.");
+	}
+}
+
+
+//--------------------------------------
+
+function YarnCell() {
+	this.y = 0;
+	this.ports = { '-':[], '+':[], 'v':[], '^':[], 'x':[], 'o':[] };
+}
+
+YarnCell.prototype.addOut = function YarnCell_addOut(dir, yarn) {
+	console.assert(dir in this.ports, "Wanted be valid direction, got '" + dir + "'.");
+	this.ports[dir].push(yarn);
+};
+
+YarnCell.prototype.canAbsorb = function YarnCell_canAbsorb(below) {
+	//NOTE: doesn't take into account yarn starts! (Though I'm not 100% sure there is a way for this to go wrong.)
+	//NOTE: doesn't take into account yarns crossing themselves! (Again, not sure this ever happens.)
+
+	//Don't collapse if in/out ports overlap:
+	if (below.ports['-'].length && this.ports['-'].length) return false;
+	if (below.ports['+'].length && this.ports['+'].length) return false;
+	if (below.ports['x'].length && this.ports['x'].length) return false;
+	if (below.ports['o'].length && this.ports['o'].length) return false;
+
+	//Don't collapse if same yarn appears in each but doesn't pass though bottom port:
+	let yarns = {};
+	//all yarns mentioned in this:
+	for (let p in this.ports) {
+		this.ports[p].forEach(function(cn){
+			yarns[cn] = true;
+		});
+	}
+	//remove yarns passing through bottom port:
+	this.ports['v'].forEach(function(cn){
+		delete yarns[cn];
+	});
+
+	//if other has any of the yarns above, can't absorb:
+	for (let p in below.ports) {
+		if (below.ports[p].some(function(cn){
+			return (cn in yarns);
+		})) {
+			return false;
 		}
 	}
 
-	//link any proud edges on left column:
-	let onLeft = face.leftEdges.slice();
-	if (this.left && this.left.rightEdges.length) {
-		console.assert(!this.leftEdges.length, "can't have left column right edges and local left edges at the same time.");
-		while (onLeft.length && this.left.rightEdges.length) {
-			//link bottom-most of each edge list:
-			onLeft.shift().link(this.left.rightEdges.shift());
+	return true;
+};
+
+YarnCell.prototype.absorb = function YarnCell_absorb(below) {
+	below.ports['^'].forEach(function(cn){
+		let idx = this.ports['v'].indexOf(cn);
+		if (idx !== -1) {
+			//block consumes yarn, so just remove the input port (it will get added back by below):
+			this.ports['v'].splice(idx, 1);
+		} else {
+			//yarn continues through to the top:
+			this.ports['^'].push(cn);
 		}
-	}
-	if (onLeft.length) {
-		console.assert(!this.left || this.left.rightEdges.length === 0, "Must not have a left column with extra edges.");
-		this.leftEdges.push(...onLeft);
-	}
+	}, this);
+	this.ports['v'].push(...below.ports['v']);
+	this.ports['+'].push(...below.ports['+']);
+	this.ports['-'].push(...below.ports['-']);
+	this.ports['x'].push(...below.ports['x']);
+	this.ports['o'].push(...below.ports['o']);
+};
 
-	//link any proud edges on right column:
-	let onRight = face.rightEdges.slice();
-	if (this.right && this.right.leftEdges.length) {
-		console.assert(!this.rightEdges.length, "can't have right column left edges and local right edges at the same time.");
-		while (onRight.length && this.right.leftEdges.length) {
-			//link bottom-most of each edge list:
-			onRight.shift().link(this.right.leftEdges.shift());
-		}
-	}
-	if (onRight.length) {
-		console.assert(!this.right || this.right.leftEdges.length === 0, "Must not have a right column with extra edges.");
-		this.rightEdges.push(...onRight);
-	}
+YarnCell.prototype.desc = function YarnCell_desc() {
+	let bits = 0;
+	bits |= (this.ports['-'].length ? 1 : 0);
+	bits |= (this.ports['+'].length ? 2 : 0);
+	bits |= (this.ports['v'].length ? 4 : 0);
+	bits |= (this.ports['^'].length ? 8 : 0);
+	const chars = [
+		' ', '╴', '╶', '─',
+		'╷', '╮', '╭', '┬',
+		'╵', '╯', '╰', '┴',
+		'│', '┤', '├', '┼'
+	];
+	return chars[bits];
+};
 
-	//store top edges for future linking:
-	this.topEdges = face.topEdges.slice();
+function LoopCell(type) {
+	this.type = type;
+	this.y = 0;
+	this.ports = { '-':[], '+':[], 'v':[], '^':[] };
+}
 
+LoopCell.prototype.addOut = YarnCell.prototype.addOut;
+
+LoopCell.prototype.canAbsorb = function LoopCell_canAbsorb(below) {
+	return false;
+};
+
+LoopCell.prototype.desc = function LoopCell_desc() {
+	return this.type;
 };
 
 //--------------------------------------
 
 function MeshMachine() {
 	this.carriers = []; //carriers, front-to-back. Each is {name:"A", yarnColumn:...(?)}
-	this.needleColumns = {};
-	this.yarnColumns = {};
+	this.beds = {
+		b:new Columns(),
+		f:new Columns()
+	};
+	this.topRow = 0;
 };
 
 //Helpers:
 
-function previousNeedle(n) {
-	let m = /^([fb]s?)([-+]?\d+)$/.match(n);
-	console.assert(m, "previousNeedle must be passed needle; got '" + n + "' instead.");
-	return m.group(1) + (parseInt(m[2])-1).toString();
-}
-
-function nextNeedle(n) {
-	let m = /^([fb]s?)([-+]?\d+)$/.match(n);
-	console.assert(m, "nextNeedle must be passed needle; got '" + n + "' instead.");
-	return m.group(1) + (parseInt(m[2])+1).toString();
-}
-
-//yarn column before given needle when knitting in direction d:
-MeshMachine.prototype.getYarnColumn = function MeshMachine_getYarnColumnBefore(d, n, create) {
-	if (typeof(create) === 'undefined') create = true;
-	if (d === '-') {
-		return this.getYarnColumnBefore('+', nextNeedle(n), create);
+MeshMachine.prototype.dump = function MeshMachine_dump() {
+	//dump to ascii grid.
+	let minIndex = Infinity;
+	let maxIndex = -Infinity;
+	for (let bn in this.beds) {
+		minIndex = Math.min(minIndex, this.beds[bn].minIndex);
+		maxIndex = Math.max(maxIndex, this.beds[bn].maxIndex);
 	}
-	if (!(n in this.yarnColumns)) {
-		if (!create) return null;
+	if (minIndex > maxIndex) return;
+	console.log("Raster is [" + minIndex + "," + maxIndex + "]x[" + 0 + "," + this.topRow + "]:");
 
-		let column = new Column();
-		this.yarnColumns[n] = column;
-
-		let left = this.getNeedleColumn(previousNeedle(n), false);
-		if (left !== null) {
-			console.assert(left.rightColumn === null, "shouldn't be linked");
-			column.left = left;
-			left.right = column;
-		}
-		let right = this.getNeedleColumn(n, false);
-		if (right !== null) {
-			console.assert(right.leftColumn === null, "shouldn't be linked");
-			column.right = right;
-			right.left = column;
-		}
+	let rasterWidth = maxIndex+1-minIndex;
+	let raster = new Array(rasterWidth * (this.topRow+1));
+	for (let i = minIndex; i <= maxIndex; ++i) {
+		this.beds['f'].getColumn(i).forEach(function(c){
+			let y = c.y;
+			let x = i - minIndex;
+			console.assert(typeof(raster[y * rasterWidth + x]) === 'undefined', "no stacks");
+			raster[y * rasterWidth + x] = c.desc();
+		});
 	}
-	return this.yarnColumns[n];
+
+	let maxY = Math.min(20, this.topRow); //DEBUG -- should be this.topRow
+	for (let y = maxY; y >= 0; --y) {
+		let row = "";
+		for (let x = 0; x < rasterWidth; ++x) {
+			if (typeof(raster[y * rasterWidth + x]) !== 'undefined') {
+				row += raster[y * rasterWidth + x];
+			} else {
+				row += ' ';
+			}
+		}
+		console.log(row);
+	}
+
 };
 
-//needle column at needle n:
-MeshMachine.prototype.getNeedleColumn = function MeshMachine_getNeedleColumn(n, create) {
-	if (typeof(create) === 'undefined') create = true;
-	if (!(n in this.needleColumns)) {
-		if (!create) return null;
+MeshMachine.prototype.addCells = function MeshMachine_addCells(b, list) {
+	console.assert(b in this.beds, "Wanted valid bed, got '" + b + "'.");
 
-		let column = new Column();
-		this.needleColumns[n] = column;
+	let y = this.topRow;
+	list.forEach(function(icell){
+		let bed = this.beds[('bed' in icell ? icell.bed : b)];
+		let column = bed.getColumn(icell.i);
+		if (column.length) {
+			let back = column[column.length-1];
+			if (back.y >= y) {
+				y = back.y;
+				if (!icell.cell.canAbsorb(back)) {
+					y = back.y + 1;
+				}
+			}
+		}
+	}, this);
 
-		let left = this.getYarnColumnBefore('+', n, false);
-		if (left !== null) {
-			console.assert(left.rightColumn === null, "shouldn't be linked");
-			column.left = left;
-			left.right = column;
+	list.forEach(function(icell){
+		let bed = this.beds[('bed' in icell ? icell.bed : b)];
+		icell.cell.y = y;
+		let column = bed.getColumn(icell.i);
+		if (column.length) {
+			let back = column[column.length-1];
+			if (back.y === y) {
+				icell.cell.absorb(back);
+				column.pop();
+			}
 		}
-		let right = this.getYarnColumnBefore('-', n, false);
-		if (right !== null) {
-			console.assert(right.leftColumn === null, "shouldn't be linked");
-			column.right = right;
-			right.left = column;
-		}
-	}
-	return this.needleColumns[n];
+		column.push(icell.cell);
+	}, this);
+
+	this.topRow = y;
 };
 
 MeshMachine.prototype.bringCarrier = function MeshMachine_moveCarrier(d, n, cn) {
 	console.assert(cn in this.carriers, "Carrier exists.");
 
 	//set up yarn for a given stitch.
-	//post-condition: needle just before n in direction d has its top-left face with yarn at exit.
-	// i.e. carrier is ready to make stitch at n in direction d
+	//post-condition: needle just before n in direction d has yarn from cn exiting via its top face.
+	// i.e. carrier is ready to make stitch at n in direction d, after (possibly) turning.
 	let c = this.carriers[cn];
 
-	let column = this.getYarnColumn(d,n);
+	let targetBed = needleBed(n);
 
-	if (!c.column) {
-		//carrier isn't in, so bring in at the top of the target column:
-
-		while (column.left && column.left.
-
+	if (!c.at) {
+		//add yarn-in cell at top of yarnBeforeIndex(d,n)
+		let cell = new YarnCell();
+		cell.addOut('^', cn);
+		this.addCells(targetBed, [{i:yarnBeforeIndex(d,n), cell:cell}]); //might increase this.topRow depending on if cell can be merged, otherwise will add *at* topRow.
+		c.at = {d:d, n:n};
+		return;
 	}
 
-	if (c.column) {
-		//carrier is in, and needs to move, potentially
-		//NOTE: it should exit from top of column.
-		assert(cn in c.column.topEdge.yarns, "carrier exits column through top edge");
+	//okay, carrier is parked at index c.at, with yarn travelling up that column to the top:
+	let atBed = needleBed(c.at.n);
+	let atIndex = yarnBeforeIndex(c.at.d, c.at.n);
+	let targetIndex = yarnBeforeIndex(d, n);
 
-	} else {
-		//carrier isn't in, add face at top of column.
 
+	//TODO: if atBed !== targetBed (...)
+	//TODO: really, in general, I guess yarn transit needs to happen on front bed with racking accounted for if there are crossings.
+
+	if (atBed !== targetBed) {
+		//move to target bed:
+		let cross = [];
+		let turn = new YarnCell();
+		turn.addOut('v', cn);
+		turn.addOut((targetBed === 'f' ? 'o' : 'x'), cn);
+		cross.push({bed:atBed, i:atIndex, cell:turn});
+		let turn2 = new YarnCell();
+		turn2.addOut((targetBed === 'f' ? 'x' : 'o'), cn);
+		turn2.addOut('^', cn);
+		cross.push({i:atIndex, cell:turn2});
+		this.addCells(targetBed, cross);
 	}
 
-};
+	let cells = [];
+	//traverse yarn right to targetIndex:
+	if (atIndex < targetIndex) {
+		for (let i = atIndex; i <= targetIndex; ++i) {
+			if (i === atIndex) {
+				//start by turning to the right:
+				let turn = new YarnCell();
+				turn.addOut('v', cn);
+				turn.addOut('+', cn);
+				cells.push({i:i, cell:turn});
+			} else if (i < targetIndex) {
+				if (i % 2 === 0) { //loop crossing
+					let miss = new LoopCell('┄');
+					miss.addOut('-', cn);
+					miss.addOut('+', cn);
+					cells.push({i:i, cell:miss});
+				} else { //yarn crossing
+					let miss = new YarnCell();
+					miss.addOut('-', cn);
+					miss.addOut('+', cn);
+					cells.push({i:i, cell:miss});
+				}
+			} else if (i === targetIndex) {
+				//end by turning up:
+				let turn = new YarnCell();
+				turn.addOut('-', cn);
+				turn.addOut('^', cn);
+				cells.push({i:i, cell:turn});
+			}
+		}
+	}
 
-/*
-MeshMachine.prototype.assertEven = function MeshMachine_assertEven() {
-	//After every stitch, there should be no left or right edges outstanding (except to null columns)
+	//traverse yarn left to targetIndex:
+	if (atIndex > targetIndex) {
+		for (let i = atIndex; i >= targetIndex; --i) {
+			if (i === atIndex) {
+				//start by turning to the left:
+				let turn = new YarnCell();
+				turn.addOut('v', cn);
+				turn.addOut('-', cn);
+				cells.push({i:i, cell:turn});
+			} else if (i > targetIndex) {
+				if (i % 2 === 0) { //loop crossing
+					let miss = new LoopCell('┄');
+					miss.addOut('+', cn);
+					miss.addOut('-', cn);
+					cells.push({i:i, cell:miss});
+				} else { //yarn crossing
+					let miss = new YarnCell();
+					miss.addOut('+', cn);
+					miss.addOut('-', cn);
+					cells.push({i:i, cell:miss});
+				}
+			} else if (i === targetIndex) {
+				//end by turning up:
+				let turn = new YarnCell();
+				turn.addOut('+', cn);
+				turn.addOut('^', cn);
+				cells.push({i:i, cell:turn});
+			}
+		}
+	}
+
+	this.addCells(targetBed, cells);
+
+	c.at = {d:d, n:n};
 };
-*/
 
 //Required functions:
 
@@ -405,12 +587,19 @@ MeshMachine.prototype.setCarriers = function MeshMachine_setCarriers(carriers) {
 
 MeshMachine.prototype.in = function MeshMachine_in(cs) { /* nothing */ };
 MeshMachine.prototype.out = function MeshMachine_out(cs) {
-	//TODO: create yarn-end faces for all carriers in cs
+	cs.forEach(function(cn){
+		console.assert(cn in this.carriers, "Trying to take out a carrier '" + cn + "' that doesn't exist.");
+		let c = this.carriers[cn];
+		if (c.at) {
+			//TODO: add some sort of yarn out cell?
+			delete c.at;
+		}
+	}, this);
 };
 MeshMachine.prototype.inhook = function MeshMachine_inhook(cs) { /* nothing */ };
 MeshMachine.prototype.releasehook = function MeshMachine_releasehook(cs) { /* nothing */ };
 MeshMachine.prototype.outhook = function MeshMachine_outhook(cs) {
-	//TODO: create yarn-end faces for all carriers in cs
+	this.out(cs); //hook doesn't matter for this code
 };
 
 MeshMachine.prototype.stitch = function MeshMachine_stitch(l, t) {
@@ -418,30 +607,64 @@ MeshMachine.prototype.stitch = function MeshMachine_stitch(l, t) {
 };
 MeshMachine.prototype.rack = function MeshMachine_rack(r) { /* nothing */ };
 
-MeshMachine.prototype.knit = function MeshMachine_knit(d, n, cs) {
-	//TODO: knit stitch
-	let column = this.getNeedleColumn(n);
+MeshMachine.prototype.knitTuck = function MeshMachine_knitTuck(d, n, cs, knitTuck) {
+	//Bring carriers on over:
+	cs.forEach(function(cn){
+		this.bringCarrier(d,n,cn);
+	}, this);
 
-	if (cs.length !== 0) {
-		this.bringCarrier(d,n,cs[0]);
-		//TODO: the rest of the yarns.
+	let cells = [];
+
+	//Turn carriers toward the needle:
+	if (cs.length) {
+		let turn = new YarnCell();
+		cs.forEach(function(cn){
+			turn.addOut('v', cn);
+			turn.addOut(d, cn);
+		}, this);
+		cells.push({i:yarnBeforeIndex(d, n), cell:turn});
 	}
 
-	//build a knit face:
-	let knit = new Face();
-	knit.leftEdges.push(new Edge(Edge.YARN));
-	knit.rightEdges.push(new Edge(Edge.YARN));
-	//custom top/bottom edges depending on whether loop is empty:
-	knit.bottomEdges.push(new Edge((column.topEdges.length === 0 ? Edge.EMPTY : Edge.LOOP)));
-	knit.topEdges.push(new Edge((cs.length === 0 ? Edge.EMPTY : Edge.LOOP)));
+	//add the face:
+	cells.push({i:needleIndex(n), cell:knitTuck});
 
-	//TODO: might need to add a bunch of empty faces to consume extra space in column(?)
-	column.addFace(knit);
+	//turn carriers back up:
+	if (cs.length) {
+		let turn = new YarnCell();
+		let nextD = d;
+		let nextN = (d === '+' ? nextNeedle(n) : previousNeedle(n));
+		cs.forEach(function(cn){
+			turn.addOut((d === '+' ? '-' : '+'), cn);
+			turn.addOut('^', cn);
+			this.carriers[cn].at = {d:nextD, n:nextN};
+		}, this);
+		cells.push({i:yarnAfterIndex(d, n), cell:turn});
+	}
+
+	this.addCells(needleBed(n), cells);
 
 };
 
+MeshMachine.prototype.knit = function MeshMachine_knit(d, n, cs) {
+	//build a knit face:
+	let knit = new LoopCell('k');
+	cs.forEach(function(cn){
+		knit.addOut((d === '+' ? '-' : '+'), cn);
+		knit.addOut(d, cn);
+	}, this);
+
+	this.knitTuck(d, n, cs, knit);
+};
+
 MeshMachine.prototype.tuck = function MeshMachine_tuck(d, n, cs) {
-	//TODO: tuck stitch face
+	//build a tuck face:
+	let tuck = new LoopCell('t');
+	cs.forEach(function(cn){
+		tuck.addOut((d === '+' ? '-' : '+'), cn);
+		tuck.addOut(d, cn);
+	}, this);
+
+	this.knitTuck(d, n, cs, tuck);
 };
 
 MeshMachine.prototype.split = function MeshMachine_split(d, n, n2, cs) {
@@ -458,4 +681,7 @@ MeshMachine.prototype.pause = function MeshMachine_pause() { /* nothing */ };
 //--------------------------------------
 //load file, pass to parser:
 const fs = require('fs');
-parseKnitout(fs.readFileSync(knitoutFile, 'utf8'), new MeshMachine());
+const machine = new MeshMachine();
+parseKnitout(fs.readFileSync(knitoutFile, 'utf8'), machine);
+
+machine.dump();
